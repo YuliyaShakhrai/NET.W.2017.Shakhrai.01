787f4c7740279e1f85c0862b5002bb37e1de100e
YuliyaShakhrai <32871561+YuliyaShakhrai@users.noreply.github.com>
2017-10-18 15:02:09 +0300

    Add files via upload

---
+++ NET.W.2017.Shakhrai.01/ArraySorting/ArraySorting.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class ArraySorting
+    {
+        public static void QuickSorting(int[] arr, int first, int last)
+        {
+            if (arr.Length == 1)
+                return;
+            int middle = arr[(last - first) / 2 + first];
+            int temp;
+            int i = first, j = last;
+            while (i <= j)
+            {
+                while (arr[i] < middle && i <= last) ++i;
+                while (arr[j] > middle && j >= first) --j;
+                if (i <= j)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    ++i;
+                    --j;
+                }
+            }
+            if (j > first) QuickSorting(arr, first, j);
+            if (i < last) QuickSorting(arr, i, last);
+        }
+
+        public static int[] MergeSorting(int[] arr)
+        {
+            if (arr.Length == 1)
+                return arr;
+            int mid_point = arr.Length / 2;
+            return Merge(MergeSorting(arr.Take(mid_point).ToArray()),
+                MergeSorting(arr.Skip(mid_point).ToArray()));
+        }
+
+        public static int[] Merge(int[] arr1, int[] arr2)
+        {
+            int a = 0, b = 0;
+            int[] result = new int[arr1.Length + arr2.Length];
+            for (int i = 0; i < arr1.Length + arr2.Length; i++)
+            {
+                if (b < arr2.Length && a < arr1.Length)
+                    if (arr1[a] > arr2[b])
+                        result[i] = arr2[b++];
+                    else
+                        result[i] = arr1[a++];
+                else
+                    if (b < arr2.Length)
+                    result[i] = arr2[b++];
+                else
+                    result[i] = arr1[a++];
+            }
+            return result;
+        }
+    }
+}
---
+++ NET.W.2017.Shakhrai.01/ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sorting;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Инициализация массива
+            int[] arr = new int[100];
+            Random rd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rd.Next(1, 1000);
+            }
+            //Вывод исходного массива в консоль
+            Console.WriteLine("Array before sorting:");
+            foreach (int a in arr)
+            {
+                Console.Write(a + " ");
+            }
+
+            //Проверяем быструю сортировку
+            //ArraySorting.QuickSorting(arr, 0, arr.Length - 1);
+            //Console.WriteLine("\nArray after quick sorting:");
+            //foreach (int a in arr)
+            //{
+            //    Console.Write(a + " ");
+            //}
+
+            //Проверяем сортировку слиянием
+            Console.WriteLine("\nArray after merge sorting:");
+            foreach (int a in ArraySorting.MergeSorting(arr))
+            {
+                Console.Write(a + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
